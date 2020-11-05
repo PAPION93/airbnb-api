c2b7dc67359fa759f0012a781a22cdfceffb5312
PAPION93~ <sjun1319@gmail.com>
2020-11-05 16:24:07 +0900

    Add room search

--- rooms/views.py
+++ rooms/views.py
@@ -1,22 +1,34 @@
+from django.core.paginator import Page
+from rest_framework import serializers
 from rest_framework.status import HTTP_403_FORBIDDEN
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.pagination import PageNumberPagination
 from rest_framework import status
 from .models import Room
 from .serializers import RoomSerializer
 
 
+class OwnPagination(PageNumberPagination):
+    page_size = 20
+
+
 class RoomsView(APIView):
     def get(self, request):
-        rooms = Room.objects.all()[:5]
-        serializer = RoomSerializer(rooms, many=True).data
-        return Response(serializer)
+
+        paginator = OwnPagination()
+        rooms = Room.objects.all()
+        results = paginator.paginate_queryset(rooms, request)
+
+        serializer = RoomSerializer(results, many=True)
+        return paginator.get_paginated_response(serializer.data)
 
     def post(self, request):
         if not request.user.is_authenticated:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
         serializer = RoomSerializer(data=request.data)
-        if (serializer.is_valid()):
+        if serializer.is_valid():
             room = serializer.save(user=request.user)
             room_serializer = RoomSerializer(room).data
             return Response(data=room_serializer, status=status.HTTP_200_OK)
@@ -25,7 +37,6 @@ class RoomsView(APIView):
 
 
 class RoomView(APIView):
-
     def get_room(self, pk):
         try:
             room = Room.objects.get(pk=pk)
@@ -49,8 +60,7 @@ class RoomView(APIView):
             if room.user != request.user:
                 return Response(status=HTTP_403_FORBIDDEN)
 
-            serializer = RoomSerializer(
-                room, data=request.data, partial=True)
+            serializer = RoomSerializer(room, data=request.data, partial=True)
             if serializer.is_valid():
                 room = serializer.save()
                 return Response(RoomSerializer(room).data)
@@ -70,3 +80,43 @@ class RoomView(APIView):
             return Response(status=status.HTTP_200_OK)
         else:
             return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(["GET"])
+def room_search(request):
+    max_price = request.GET.get("max_price", None)
+    min_price = request.GET.get("min_price", None)
+    beds = request.GET.get("beds", None)
+    bedrooms = request.GET.get("bedrooms", None)
+    bathrooms = request.GET.get("bathrooms", None)
+    lat = request.GET.get("lat", None)
+    lng = request.GET.get("lng", None)
+
+    filter_kwargs = {}
+    if max_price is not None:
+        filter_kwargs["price__lte"] = max_price
+    if min_price is not None:
+        filter_kwargs["price__gte"] = min_price
+    if beds is not None:
+        filter_kwargs["beds__gte"] = beds
+    if bedrooms is not None:
+        filter_kwargs["bedrooms__gte"] = bedrooms
+    if bathrooms is not None:
+        filter_kwargs["bathrooms__gte"] = bathrooms
+
+    paginator = OwnPagination()
+
+    if lat is not None and lng is not None:
+        filter_kwargs["lat__gte"] = float(lat) - 0.005
+        filter_kwargs["lat__lte"] = float(lat) + 0.005
+        filter_kwargs["lng__gte"] = float(lng) - 0.005
+        filter_kwargs["lng__lte"] = float(lng) + 0.005
+
+    try:
+        rooms = Room.objects.filter(**filter_kwargs)
+    except:
+        rooms = Room.objects.all()
+
+    results = paginator.paginate_queryset(rooms, request)
+    serializer = RoomSerializer(results, many=True)
+    return paginator.get_paginated_response(serializer.data)
